a7bc608a8ace180f7900b95cc51ed4f7fa7d7fb6
AlexPanno <alexpannno@gmail.com>
2020-06-04 19:20:03 +0300

    Создана секция "Our services"
    Добавлен слайдер Slick JS.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*=================jQuery=================*/
+
+$('.services-slider').slick({
+  slidesToShow: 3,
+  infinite: false,
+  centerMode: true,
+  centerPadding: '0',
+  initialSlide: 1,
+})
