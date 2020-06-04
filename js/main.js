d2c88f823bdd74e06804851c98443dec669eb095
AlexPanno <alexpannno@gmail.com>
2020-06-04 21:12:28 +0300

    Создана секция "FAQ".

--- js/main.js
+++ js/main.js
@@ -9,3 +9,17 @@ $('.services-slider').slick({
   centerPadding: '0',
   initialSlide: 1,
 })
+
+/*=================Native JavaScript=================*/
+
+// Question buttons on section "FAQ"
+
+let questionButtons = document.querySelectorAll('.question-button');
+
+questionButtons.forEach((item) => {
+  item.onclick = function () {
+    let currentAnswer = this.parentElement.nextElementSibling;
+    currentAnswer.classList.toggle("active-answer");
+    this.classList.toggle("active-question-button");
+  }
+});
