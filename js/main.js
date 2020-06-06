67efae621b3c1fc7a4a2ad84452d1937f3577fca
AlexPanno <alexpannno@gmail.com>
2020-06-06 12:44:19 +0300

    Добавление адаптивного дизайна

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/*=================jQuery=================*/
+/*================= jQuery =================*/
 
 $('.services-slider').slick({
   slidesToShow: 3,
@@ -8,9 +8,31 @@ $('.services-slider').slick({
   centerMode: true,
   centerPadding: '0',
   initialSlide: 1,
+  responsive: [
+    {
+      breakpoint: 1281,
+      settings: {
+        slidesToShow: 1,
+        centerPadding: '28%'
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 1,
+        centerPadding: '22%'
+      }
+    },
+    {
+      breakpoint: 601,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ],
 })
 
-/*=================Native JavaScript=================*/
+/*================= Native JavaScript =================*/
 
 // Question buttons on section "FAQ"
 
@@ -23,3 +45,12 @@ questionButtons.forEach((item) => {
     this.classList.toggle("active-question-button");
   }
 });
+
+// Touch menu button
+
+let touchMenuButton = document.querySelector('.touch-menu');
+let headerNavUl = document.querySelector('.nav-ul');
+
+touchMenuButton.onclick = function () {
+  headerNavUl.classList.toggle('active-nav-ul');
+}
